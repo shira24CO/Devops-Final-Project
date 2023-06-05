768228abaddb7d17ece8677571ab6bdcfe59830e
shoham <moyal.shoham@gmail.com>
2023-06-05 14:03:02 +0300

    circleci - config.yml5

--- tests/test.test.js
+++ tests/test.test.js
@@ -9,6 +9,6 @@ describe('Test suite 1:', () => {
     })
     test('test 2:', async () => {
         const res = await request(app).get('/1234')
-        expect(res.statusCode).toEqual(404)
+        expect(res.statusCode).toEqual(400)
     })
 })
