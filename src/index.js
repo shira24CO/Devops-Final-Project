85c178e65c9a5a6d2f590c269b3214ee4bb7d1f3
shoham <moyal.shoham@gmail.com>
2023-06-05 20:09:11 +0300

    new

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import express from 'express';
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
 
 const port = process.env.PORT || 4000;
 const app = express();
@@ -6,6 +9,10 @@ const app = express();
 app.get('/', (req, res ) => {
     console.log('A new request has arrived to index.js');
     res.send('Hello From the server');
+});
+app.get('/Register', (req, res ) => {
+
+    res.sendFile(path.resolve('Register.js'));
 });
 
 app.listen(port, () => {
